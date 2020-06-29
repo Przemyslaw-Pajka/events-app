600521a44310f0db8268f05d4f48d91f72d23350
Przemysław <64286950+Przemyslaw-Pajka@users.noreply.github.com>
2020-06-29 08:44:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-//TODO: Zabezpieczyć API Key
-//TODO: Na samum końcu Code Review
-//TODO: Upload na serwer
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
